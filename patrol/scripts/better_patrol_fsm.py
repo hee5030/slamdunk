0b98df407aab8c8a555827b37cd31af3b8ebbfcc
root1 <joon604444@gmail.com>
2023-04-05 09:57:56 +0900

    0405

---
+++ patrol/scripts/better_patrol_fsm.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import rospy
+from smach import StateMachine # <1>
+from smach_ros import SimpleActionState # <2>
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+
+# waypoints = [
+#     ['one', (2.1, 2.2), (0.0, 0.0, 0.0, 1.0)],
+#     ['two', (6.5, 4.43), (0.0, 0.0, -0.984047240305, 0.177907360295)]
+# ]
+waypoints = [  # <1>
+    ['one', (2.1, 2.2), (0.0, 0.0, 0.0, 1.0)],
+    ['two', (2.1, 5.43), (0.0, 0.0, -0.984047240305, 0.177907360295)],
+    ['three', (5.3, 5.4), (0.0, 0.0, 0.0, 1.0)],
+    ['four', (5.2, 0.1), (0.0, 0.0, 0.0, 1.0)],
+    ['five', (2.1, 2.2), (0.0, 0.0, 0.0, 1.0)]
+]
+
+if __name__ == '__main__':
+    rospy.init_node('patrol')
+
+    patrol = StateMachine(['succeeded', 'aborted', 'preempted'])
+    with patrol:
+        for i,w in enumerate(waypoints):
+            goal_pose = MoveBaseGoal()
+            goal_pose.target_pose.header.frame_id = 'map'
+            goal_pose.target_pose.pose.position.x = w[1][0]
+            goal_pose.target_pose.pose.position.y = w[1][1]
+            goal_pose.target_pose.pose.position.z = 0.0
+            goal_pose.target_pose.pose.orientation.x = w[2][0]
+            goal_pose.target_pose.pose.orientation.y = w[2][1]
+            goal_pose.target_pose.pose.orientation.z = w[2][2]
+            goal_pose.target_pose.pose.orientation.w = w[2][3]
+
+            StateMachine.add(w[0],
+                             SimpleActionState('move_base',
+                                               MoveBaseAction,
+                                               goal=goal_pose),
+                             transitions={'succeeded':waypoints[(i + 1) % \
+                               len(waypoints)][0]})
+
+    patrol.execute()
+    
